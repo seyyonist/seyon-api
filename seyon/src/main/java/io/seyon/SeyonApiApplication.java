7c5156bdb69ddacf960f53d61563267e8fd715b2
nvijaykarthik <nvijaykarthik@gmail.com>
2018-08-11 19:41:07 +0530

    mergin PCF cloud changes

--- seyon/src/main/java/io/seyon/SeyonApiApplication.java
+++ seyon/src/main/java/io/seyon/SeyonApiApplication.java
@@ -15,13 +15,6 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.VendorExtension;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-
 @SpringBootApplication
 public class SeyonApiApplication {
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
@@ -33,13 +26,7 @@ public class SeyonApiApplication {
 		SpringApplication.run(SeyonApiApplication.class, args);
 	}
 
-	@Bean
-	public Docket productApi() {
-		log.debug("Creating swagger docket");
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("io.seyon.resource")).build().apiInfo(metaData());
-	}
-
+	
 	@Bean
 	public WebMvcConfigurer interceptorConfigurer() {
 		return new WebMvcConfigurer() {
@@ -48,21 +35,14 @@ public class SeyonApiApplication {
 
 			@Override
 			public void addInterceptors(InterceptorRegistry registry) {
+				log.info("Adding interceptors");
 				registry.addInterceptor(securityInterceptor).excludePathPatterns(seyonProperties.getAuthExcludeUrl());
 				WebMvcConfigurer.super.addInterceptors(registry);
 			}
 		};
 	}
 
-	private ApiInfo metaData() {
-		Collection<VendorExtension> coll = new ArrayList<VendorExtension>();
-		log.debug("Swagger API info");
-		ApiInfo apiInfo = new ApiInfo("Rest API For Seyon", "REST API For Seyon App", "1.0", "Terms of service",
-				new Contact("ramlaxmi", "https://github.com/ramlaxmi/", "krithika6686@gmail.com"), "MIT",
-				"https://opensource.org/licenses/MIT", coll);
-		return apiInfo;
-	}
-
+	
 	//@Bean  // deactivating the ip based restriction
 	public FilterRegistrationBean<RemoteAddrFilter> remoteAddressFilter() {
 
